eb88d3155f9be4d351b699975388710d9339d5c5
Karina Carrasco <karict@gmail.com>
2022-11-07 16:33:28 -0400

    AlertDialog

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Label } from './components/Label';
 import { SimpleDialogProps } from './components/SimpleDialog';
 import { Modal } from './components/modal';
-import { AlertDialog } from './components/AlertDialog';
+import AlertDialog  from './components/AlertDialog';
 
 //export default SimpleDialogProps;
 export default AlertDialog;
