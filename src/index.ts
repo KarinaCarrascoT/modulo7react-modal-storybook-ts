69e72c03004fe386ae68ac2bd4bef36b6448daad
Karina Carrasco <karict@gmail.com>
2022-11-07 15:51:17 -0400

    index.ts export default

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Label } from './components/Label';
 import { Modal } from './components/modal';
+
 import { SimpleDialogProps } from './components/SimpleDialog';
 
 export default SimpleDialogProps;
